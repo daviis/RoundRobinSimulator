418cd0167fe541ef8ba01b576774bbe61ab22eaf
daviis <daviis@luther.edu>
2014-03-16 15:05:59 -0500

    got printing working for main clocking

--- simulator.cpp
+++ simulator.cpp
@@ -13,17 +13,19 @@ class Program{
 	int runCycles, waitCycles, ioCycles;
 	queue<int>* lifeCycle;
     public:
-	Program(queue<int>*);
+	Program(queue<int>*, int);
 	bool incRC();
 	bool incWC();
-	bool incIOC();	
+	bool incIOC();
+	int progId;	
 };
 
-Program::Program(queue<int>* aQue){
+Program::Program(queue<int>* aQue, int id){
 	runCycles = 0;
 	waitCycles = 0;
 	ioCycles = 0;
 	lifeCycle = aQue;
+	progId = id;
 }
 
 bool Program::incRC(){
@@ -32,7 +34,7 @@ bool Program::incRC(){
 
 queue<int>* breakLine(string);
 vector<Program*>* runSimulation(queue<Program*>*, int);
-
+void printStates(queue<Program*>, queue<Program*>);
 
 int main(int argc, char* argv[]){
 	//some config stuff
@@ -45,9 +47,11 @@ int main(int argc, char* argv[]){
 	string line;
 
 	//read the file and make program objects out of it	
+	int idCount = 1;
 	while(getline(simFile, line)){
 		queue<int>* clockCounts = breakLine(line);
-		progQueue->push(new Program(clockCounts));
+		progQueue->push(new Program(clockCounts, idCount));
+		idCount++;
 	}
 	vector<Program*>* finalCounts = runSimulation(progQueue, timeQuantum);	
 	
@@ -56,20 +60,36 @@ int main(int argc, char* argv[]){
 
 vector<Program*>* runSimulation(queue<Program*>* runQueue, int quant){
 	vector<Program*>* outputVec = new vector<Program*>;
+	queue<Program*>* ioQue = new queue<Program*>;
 	while(!runQueue->empty()){
 		int currClock = 0;
 		Program* runningProg = runQueue->front();
+		cout << runningProg->progId << " enters running state" << endl;
 		runQueue->pop();
 		while(currClock < quant && runningProg->incRC()){
 			//inc waitCount for all in queue; 
+			cout << runningProg->progId << " is running" << endl;
+			queue<Program*> printQueue (*runQueue);
+			queue<Program*> ioQueue (*ioQue);
+			printStates(printQueue, ioQueue);
 			currClock++;
-			cout << currClock << endl;
 		}
 	}
 	return outputVec;
 }
 
-
+void printStates(queue<Program*> printQ, queue<Program*> ioQ){
+	while(!printQ.empty()){
+		Program* p = printQ.front();
+		printQ.pop();	
+		cout << "\t\t" << p->progId << " is waiting" << endl;
+	}
+	while(!ioQ.empty()){
+		Program* p = ioQ.front();
+		ioQ.pop();
+		cout << '\t' << p->progId << " is doing io" << endl;
+	}
+}
 queue<int>* breakLine(string line){
 	char aChar = ' ';
 	istringstream linestream(line);
